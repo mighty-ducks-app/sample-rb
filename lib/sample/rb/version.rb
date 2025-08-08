9007e70e9ba80fbff9ac4aeddc5ba9b94f877154
David Rouchy <david@rouchy.dev>
2025-08-08 10:10:07 +0100

    Basic gem project

---
+++ lib/sample/rb/version.rb
@@ -0,0 +1,7 @@
+ # frozen_string_literal: true
+
+module Sample
+  module Rb
+    VERSION = "0.2.0"
+  end
+end
---
+++ spec/sample/rb_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Sample::Rb do
+  it "has a version number" do
+    expect(Sample::Rb::VERSION).not_to be nil
+  end
+end
